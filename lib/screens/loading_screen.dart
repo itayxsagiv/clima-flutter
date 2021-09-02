6da6945fa3039cc60587bbcb0efa381ad2d0fe00
itayxsagiv <itayxsagiv@gmail.com>
2021-09-03 00:03:47 +0300

    Step 8 - HTTP get

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,6 +10,21 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      print(response.body);
+      var decodedData = jsonDecode(response.body);
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print('$temperature $condition $cityName');
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
@@ -22,6 +39,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: Text('Get Location'),
